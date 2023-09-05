19192db39f0d83f8213995504a26f8f02aad6851
SusidkaVladislav <vladislav.susidka@gmail.com>
2023-09-05 20:19:27 +0300

    Created material-desugn element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import TreeView from '@mui/lab/TreeView'
+import TreeItem from '@mui/lab/TreeItem'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        className="head"
+        style={{
+          width: 'fit-content',
+          margin: 'auto',
+        }}
+      >
+        <h1
+          style={{
+            color: 'green',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          GeeksforGeeks
+        </h1>
+        <strong>React MUI TreeView API</strong>
+      </div>
+
+      <TreeView
+        aria-label="Tutorials navigator"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{
+          margin: 'auto',
+          flexGrow: 1,
+          width: 'fit-content',
+        }}
+      >
+        <TreeItem nodeId="1" label="Data Structures">
+          <TreeItem nodeId="2" label="Array" />
+          <TreeItem nodeId="3" label="Max Heap" />
+          <TreeItem nodeId="4" label="Stack" />
+        </TreeItem>
+        <TreeItem nodeId="5" label="Algorithms">
+          <TreeItem nodeId="10" label="Gready" />
+          <TreeItem nodeId="6" label="Graph">
+            <TreeItem nodeId="8" label="DFS" />
+            <TreeItem nodeId="8" label="BFS" />
+          </TreeItem>
+        </TreeItem>
+      </TreeView>
     </div>
   );
 }
